2f04383741460485151b659e49516ec947fc701b
Artur Tenca <arturtenca1@gmail.com>
2024-09-21 17:50:31 -0300

    fazendo pagina de cadastro

--- src/routes/CadastrarUsuario.jsx
+++ src/routes/CadastrarUsuario.jsx
@@ -1,84 +1,90 @@
-import {useParams,Link, useNavigate} from 'react-router-dom'
+import { useParams, Link, useNavigate } from 'react-router-dom'
 import { ImCancelCircle } from "react-icons/im";
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { CadastrarUsuarioStyle } from '../css/CadastrarUsuarioStyle';
 
 
-const CadastrarUsuario=()=>{
+const CadastrarUsuario = () => {
 
-    let {id} =useParams();
-    const [usuarios,setUsuarios]= useState({
+    let { id } = useParams();
+    const [usuarios, setUsuarios] = useState({
         id,
-        usuario:'',
-        senha:'',
+        usuario: '',
+        senha: '',
     });
 
-     const navigate = useNavigate();
+    const navigate = useNavigate();
 
-    let metodo ="post";
-    if(id){
-        metodo="put";
+    let metodo = "post";
+    if (id) {
+        metodo = "put";
     }
-     const handleChange=(e)=>{
-        setUsuarios({...usuarios, [e.target.name]: e.target.value});
-     }
+    const handleChange = (e) => {
+        setUsuarios({ ...usuarios, [e.target.name]: e.target.value });
+    }
 
-     const handleSubmit=(e)=>{
-    
+    const handleSubmit = (e) => {
+
         e.preventDefault();
-        fetch(`http://localhost:5000/usuarios/${id ? id:''}`,{
-        method: metodo,
-        headers: {
-            'Content-Type':'application/json',
-        },
-        body: JSON.stringify(usuarios),
-        }).then(()=> {
+        fetch(`http://localhost:5000/usuarios/${id ? id : ''}`, {
+            method: metodo,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(usuarios),
+        }).then(() => {
             navigate("/login")
         });
-     }
-
-     useEffect(()=>{
-        if(id) {
-        fetch(`http://localhost:5000/usuarios/${id}`)
-        .then((res)=>{
-            return res.json();
-        })
-        .then((data)=>{
-            setUsuarios(data);
-        });
     }
-     },[id])
+
+    useEffect(() => {
+        if (id) {
+            fetch(`http://localhost:5000/usuarios/${id}`)
+                .then((res) => {
+                    return res.json();
+                })
+                .then((data) => {
+                    setUsuarios(data);
+                });
+        }
+    }, [id])
 
 
     return (
-        <section className="usuario">
+        <CadastrarUsuarioStyle>
+            <section className="usuario">
+                <div className="container-login">
+                    <div className="login">
+                            <Link className='x' to="/login">
+                            <ImCancelCircle />
+                             </Link>
+                        <form onSubmit={handleSubmit}>
+                            <span className="titulo">Cadastre-se</span>
+                            <input
+                                type="text"
+                                name="usuario"
+                                placeholder="Digite seu Usuário"
+                                value={usuarios.usuario}
+                                onChange={handleChange}
+                            />
+                            
+                            <input
+                                type="password"
+                                name="senha"
+                                placeholder="Digite sua senha"
+                                value={usuarios.senha}
+                                onChange={handleChange}
+                            />
 
-            <form onSubmit={handleSubmit}>
-                <h1>Cadastrar Usuário</h1>
-                <input
-                    type="text"
-                    name="usuario"
-                    placeholder="Digite seu Usuário"
-                    value={usuarios.usuario}
-                    onChange={handleChange}
-                />
+                            <button type="submit">Cadastrar</button>
 
-                <input
-                    type="password"
-                    name="senha"
-                    placeholder="Digite sua senha"
-                    value={usuarios.senha}
-                    onChange={handleChange}
-                />
+                        </form>
+                    </div>
+                </div>
+            </section>
+        </CadastrarUsuarioStyle>
 
-                <button type="submit">Cadastrar</button>
-                <Link to="/login">
-                    <ImCancelCircle />
-                </Link>
-
-
-
-            </form>
-        </section>
     )
 }
 export default CadastrarUsuario
+
